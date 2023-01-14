518fe0d0655ef1ac736dc0218e010d82529973fd
avalanche <anwentao1@gmail.com>
2023-01-14 22:10:02 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         ZhihuVideoRemover
+// @namespace    https://github.com/TimeSea05/zhihu-video-remover
+// @version      0.1
+// @description  remove annoying zhihu videos
+// @author       Avalanche
+// @match        *://www.zhihu.com*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function hasVideoIndex(card) {
+    return JSON.parse(card.childNodes[0].getAttribute('data-za-extra-module'))['card']['has_video']
+  }
+
+  function removeZhihuVideosAtIndex() {
+    const recommendCardSelector = 'div[class="Card TopstoryItem TopstoryItem-isRecommend"]'
+    let cards = document.querySelectorAll(recommendCardSelector)
+  
+    for (let card of cards) {
+      if (hasVideoIndex(card)) {
+        card.remove()
+      }
+    }
+  }
+
+  function hasVideoAnswers(answer) {
+    for (let node of answer.childNodes) {
+      if (node.className === 'VideoAnswerPlayer') {
+        return true
+      }
+    }
+  
+    return false
+  }
+
+  function removeZhihuVideosAtAnswers() {
+    const answerSelector = 'div[class="ContentItem AnswerItem"]'
+    let answers = document.querySelectorAll(answerSelector)
+  
+    for (let answer of answers) {
+      if (hasVideoAnswers(answer)) {
+        answer.remove()
+      }
+    }
+  }
+
+  function removeZhihuVideos() {
+    // at index page or '/questions' page
+    if (document.URL === "https://www.zhihu.com/") {
+      removeZhihuVideosAtIndex()
+    } else if (document.URL.indexOf("https://www.zhihu.com/question/") !== -1) {
+      removeZhihuVideosAtAnswers()
+    }
+  }
+
+  document.addEventListener('wheel', event => {
+    removeZhihuVideos()
+  })
+
+  removeZhihuVideos()
+})();
