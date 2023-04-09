5143c25a0eaaceba43f7e7fae3b9b2730c9b183a
avalanche <anwentao1@gmail.com>
2023-04-09 16:35:32 +0800

    fix bug: script don't work on question page

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  remove annoying zhihu videos
 // @author       Avalanche
-// @match        *://www.zhihu.com*
+// @match        *://www.zhihu.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -13,17 +13,25 @@
   'use strict';
 
   function hasVideoAtIndex(card) {
-    return JSON.parse(card.childNodes[0].getAttribute('data-za-extra-module'))['card']['has_video']
+    return JSON.parse(card.childNodes[0].getAttribute('data-za-extra-module')).card.has_video
   }
 
   function removeZhihuVideosAtIndex() {
     const recommendCardSelector = 'div[class="Card TopstoryItem TopstoryItem-isRecommend"]'
     let cards = document.querySelectorAll(recommendCardSelector)
-  
+
     for (let card of cards) {
       if (hasVideoAtIndex(card)) {
         card.remove()
       }
+    }
+  }
+
+  function removeZhihuAdsAtIndex() {
+    const adCardSelector = "div[class=Card TopstoryItem TopstoryItem--advertCard TopstoryItem-isRecommend]"
+    let adCards = document.querySelectorAll(adCardSelector)
+    for (let card of adCards) {
+      card.remove()
     }
   }
 
@@ -33,14 +41,14 @@
         return true
       }
     }
-  
+
     return false
   }
 
   function removeZhihuVideosAtAnswers() {
     const answerSelector = 'div[class="ContentItem AnswerItem"]'
     let answers = document.querySelectorAll(answerSelector)
-  
+
     for (let answer of answers) {
       if (hasVideoAtAnswers(answer)) {
         answer.remove()
@@ -52,6 +60,7 @@
     // at index page or '/questions' page
     if (document.URL === "https://www.zhihu.com/") {
       removeZhihuVideosAtIndex()
+      removeZhihuAdsAtIndex()
     } else if (document.URL.indexOf("https://www.zhihu.com/question/") !== -1) {
       removeZhihuVideosAtAnswers()
     }
